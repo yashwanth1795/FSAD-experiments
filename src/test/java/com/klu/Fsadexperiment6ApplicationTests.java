e9cc86d6a9fc056c8e400013e078b7ed35714922
Gowni Yashwanth <gowniyashwanth72@gmail.com>
2026-02-19 22:32:30 +0530

    19/2/26

---
+++ src/test/java/com/klu/Fsadexperiment6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fsadexperiment6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
